54d58d9d6e01aa53854676610a97c6fea75e4efe
glen <glen84_dr@hotmail.com>
2023-11-29 17:48:53 +0400

    tableau organique

--- dashboard_app/views.py
+++ dashboard_app/views.py
@@ -64,10 +64,12 @@ def tableau_de_bord_perso(request):
 
 
 def subventions(request):
+    base_template = "dashboard/partial.html" if request.htmx else "dashboard/base.html"
     context = {
+        'base_template': base_template,
         'name': request.user.email if request.user.is_authenticated else 'Anonymous',
     }
-    return render(request, 'subventions.html', context=context)
+    return render(request, 'dashboard/pages_html/subventions.html', context=context)
 
 
 def organigramme(request):
