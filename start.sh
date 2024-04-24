77e2f40fba01c572582e86a35add701ba41b11f0
Jonas Legion <jturbeaux@pm.me>
2024-04-24 11:48:09 +0200

    migrate

--- start.sh
+++ start.sh
@@ -8,7 +8,9 @@ echo "Poetry install ok"
 poetry run python3 manage.py migrate
 #poetry run python3 manage.py collectstatic --noinput
 #poetry run python3 manage.py install
+
 poetry run python3 manage.py runserver 0.0.0.0:80
+
 #sleep infinity
 #echo "Run GUNICORN"
 #poetry run gunicorn fedowallet_django.wsgi --log-level=debug --log-file /fedow/www/gunicorn.logs -w 3 -b 0.0.0.0:8000
