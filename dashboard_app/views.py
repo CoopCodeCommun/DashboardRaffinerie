1cb50d40b5de307d6a9155abb03bac310eae09d7
glen <glen84_dr@hotmail.com>
2024-05-03 00:23:42 +0200

    front correction

--- dashboard_app/views.py
+++ dashboard_app/views.py
@@ -1316,7 +1316,3 @@ class lazy_loading_profil_image(APIView):
             'contact': Contact.objects.get(pk=uuid)
         }
         return render(request, 'htmx/lazy_loading.html', context=context)
-
-
-def test_columns(request):
-    return render(request, 'dashboard/pages_html/gestion_colonnes.html')
